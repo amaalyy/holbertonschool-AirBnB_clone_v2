bd4d15af00c6e65b7a53799520209078e95760d6
amaalyy <amal.elzowawi@gmail.com>
2024-02-04 15:17:53 +0100

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,19 +4,21 @@ a script that starts a Flask web application:
 """
 from flask import Flask, render_template
 from models import storage
-from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = list(storage.all("State").value())
+    states = list(storage.all(State).values())
     states.sort(key=lambda state: state.name)
+
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def closedb(exception):
+def teardown(exception):
     storage.close()
 
 
