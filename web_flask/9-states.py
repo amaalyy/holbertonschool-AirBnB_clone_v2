b5ba03b5648b4091edd5b5e612c93bf8609ef060
amaalyy <amal.elzowawi@gmail.com>
2024-02-11 19:27:57 +0100

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3
-"""
-a script that starts a Flask web application:
-"""
+"""Script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
-from models.state import State
-from models.city import City
+
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def state_id(id=""):
-    state = []
-    data = storage.all(State)
-    for key, value in data.items():
-        state.append(value)
-    if id:
-        key = "State." + id
-        if key not in data:
-            state = ""
+@app.route("/states", strict_slashes=False)
+def states_list():
+    """returns a list of all states in the database"""
+    from models import storage
+    from models.state import State
 
-    cities = []
-    dt = storage.all(City)
-    for key, value in dt.items():
-        cities.append(value)
-    return render_template("9-states.html", id=id, state=state, cities=cities)
+    states = storage.all(State).values()
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def state(id):
+    """returns a list of all states in the database"""
+    from models import storage
+    from models.state import State
+
+    states = storage.all(State).values()
+    for state in states:
+        if state.id == id:
+            return render_template("9-states.html", state=state, mode="id")
+    return render_template("9-states.html", states=None, mode="id")
 
 
 @app.teardown_appcontext
 def teardown(exception):
+    """Remove the current SQLAlchemy session"""
+    from models import storage
+
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
