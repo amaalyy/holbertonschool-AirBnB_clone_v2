123d5e73c59f510856dce5225328f8c12e90f473
amaalyy <amal.elzowawi@gmail.com>
2024-02-11 19:31:34 +0100

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
+"""Script that starts a Flask web application"""
 from flask import Flask, render_template
 
 
@@ -8,16 +8,17 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage"""
+    """returns a list of all states in the database"""
     from models import storage
+    from models.state import State
 
-    states = storage.all("State")
+    states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session"""
+def teardown_db(exception):
+    """Closes the storage on teardown"""
     from models import storage
 
     storage.close()
